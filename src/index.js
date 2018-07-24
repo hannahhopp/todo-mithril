3bb27b794624dbf405a68d5b01175ed33805a49c
Hann Hopp <hannah.hopp@gmail.com>
2018-07-24 11:28:36 -0700

    styling added to list

---
+++ src/index.js
@@ -0,0 +1,4 @@
+const m = require("mithril");
+const UserList = require("./views/UserList");
+
+m.mount(document.body, UserList);
--- src/views/UserList.js
+++ src/views/UserList.js
@@ -2,7 +2,9 @@ const m = require("mithril");
 const User = require("../models/User");
 
 module.exports = {
-    view : function () {
+    oninit  : User.loadList,
+
+    view    : function () {
         return m(".user-list", User.list.map((user) => {
             const { firstName, lastName } = user;
             return m(".user-list-item", `${firstName} ${lastName}`);
